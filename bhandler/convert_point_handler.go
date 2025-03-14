7f505c11ebcbdbf063031381114eee2dec8dbf56
流光 <67579722@qq.com>
2025-03-14 14:18:12 +0800

    fix点数兑换

--- bhandler/convert_point_handler.go
+++ bhandler/convert_point_handler.go
@@ -10,8 +10,11 @@ import (
 
 // 兑换结果
 const (
-	convertSuccess byte = 0 //兑换成功
-	convertFailed  byte = 1 //兑换失败
+	convertSuccess        byte = 0 //兑换成功
+	convertAlreadyGet     byte = 1 //已经购买过了
+	convertNotEnoughPoint byte = 2 //点数不够
+	convertInvalidPoint   byte = 3 //编码错误
+	convertOtherErr       byte = 4 //其他错误
 )
 
 // ConvertPointHandler 处理点数兑换
@@ -54,11 +57,6 @@ func (h *ConvertPointHandler) GetResponse(request *common.BillingPacket) *common
 	mGoodsNumber := packetReader.ReadUint16()
 	//需要兑换的点数
 	needPoint := int(mGoodsNumber)
-	//每次兑换点数上限 u2
-	var maxPoint = 0xffff //65535
-	if needPoint > maxPoint {
-		needPoint = maxPoint
-	}
 	//初始化兑换的结果
 	convertResult := convertSuccess
 	convertResultText := "success"
@@ -66,47 +64,50 @@ func (h *ConvertPointHandler) GetResponse(request *common.BillingPacket) *common
 	userPoint := 0
 	account, err := models.GetAccountByUsername(h.Resource.Db, string(username))
 	if err != nil {
-		convertResult = convertFailed
+		convertResult = convertOtherErr
 		convertResultText = "get account info error"
 		h.Resource.Logger.Error("get account:" + string(username) + " info failed: " + err.Error())
 	}
 	if account != nil {
 		userPoint = account.Point
 	}
-	//初始化剩余点数
-	leftPoint := userPoint
-	if leftPoint < 0 {
-		leftPoint = 0
-	}
-	//初始化兑换的点数
-	realPoint := needPoint
-	//兑换的点数不能超过用户拥有的点数,如果超过则截断
-	if realPoint > leftPoint {
-		realPoint = leftPoint
-	}
 	//兑换的点数必须是正整数
-	if realPoint <= 0 {
-		convertResult = convertFailed
+	if needPoint <= 0 {
+		convertResult = convertInvalidPoint
 		convertResultText = "invalid need point value"
+	} else if needPoint > userPoint {
+		//点数不足
+		convertResult = convertNotEnoughPoint
+		convertResultText = "point not enough"
 	}
+	//剩余点数
+	leftPoint := userPoint
 	// 执行兑换
 	if convertResult == convertSuccess {
-		if err := models.ConvertUserPoint(h.Resource.Db, string(username), realPoint); err != nil {
+		if err := models.ConvertUserPoint(h.Resource.Db, string(username), needPoint); err != nil {
 			h.Resource.Logger.Error("convert point failed: " + err.Error())
-			convertResult = convertFailed
+			convertResult = convertOtherErr
 			convertResultText = "convert point failed"
 		} else {
-			leftPoint -= realPoint
+			leftPoint -= needPoint
 		}
 	}
 	//日志记录
-	h.Resource.Logger.Info(fmt.Sprintf("user [%s] %s(ip: %s) "+
-		"point total [%d], need point [%d],"+
-		" (%d - %d = %d): %s",
-		username, charName, loginIP,
-		userPoint, needPoint,
-		userPoint, realPoint, leftPoint,
-		convertResultText))
+	if convertResult == convertSuccess {
+		h.Resource.Logger.Info(fmt.Sprintf("user [%s] %s(ip: %s) "+
+			"point total [%d], need point [%d],"+
+			" (%d - %d = %d): %s",
+			username, charName, loginIP,
+			userPoint, needPoint,
+			userPoint, needPoint, leftPoint,
+			convertResultText))
+	} else {
+		h.Resource.Logger.Info(fmt.Sprintf("user [%s] %s(ip: %s) "+
+			"point total [%d], need point [%d], error: %s",
+			username, charName, loginIP,
+			userPoint, needPoint,
+			convertResultText))
+	}
 	// 数据包组合
 	//Packets::BLRetAskBuy
 	opData := make([]byte, 0, 1+usernameLength+21+1+4+2+4+2)
@@ -114,32 +115,31 @@ func (h *ConvertPointHandler) GetResponse(request *common.BillingPacket) *common
 	opData = append(opData, username...)
 	opData = append(opData, orderIDBytes...)
 	opData = append(opData, convertResult)
-	//写入剩余点数:u4(此值不会被用到,服务端以购买的数量来进行计算)
-	leftPointU4 := leftPoint * 1000
-	for i := 0; i < 4; i++ {
-		tmpValue := leftPointU4
-		movePos := (3 - i) * 8
-		if movePos > 0 {
-			tmpValue >>= movePos
+	if convertResult <= convertAlreadyGet {
+		//写入剩余点数:u4(此值不会被用到,服务端以购买的数量来进行计算)
+		leftPointU4 := leftPoint * 1000
+		for i := 0; i < 4; i++ {
+			tmpValue := leftPointU4
+			movePos := (3 - i) * 8
+			if movePos > 0 {
+				tmpValue >>= movePos
+			}
+			opData = append(opData, byte(tmpValue&0xff))
 		}
-		opData = append(opData, byte(tmpValue&0xff))
-	}
-	//mGoodsTypeNum:u2
-	opData = append(opData, byte((mGoodsTypeNum&0xff00)>>8), byte(mGoodsTypeNum&0xff))
-	// 写入mGoodsType:u4
-	for i := 0; i < 4; i++ {
-		tmpValue := mGoodsType
-		movePos := (3 - i) * 8
-		if movePos > 0 {
-			tmpValue >>= movePos
+		//mGoodsTypeNum:u2
+		opData = append(opData, byte((mGoodsTypeNum&0xff00)>>8), byte(mGoodsTypeNum&0xff))
+		// 写入mGoodsType:u4
+		for i := 0; i < 4; i++ {
+			tmpValue := mGoodsType
+			movePos := (3 - i) * 8
+			if movePos > 0 {
+				tmpValue >>= movePos
+			}
+			opData = append(opData, byte(tmpValue&0xff))
 		}
-		opData = append(opData, byte(tmpValue&0xff))
+		//消耗的点数:u2
+		opData = append(opData, byte((needPoint&0xff00)>>8), byte(needPoint&0xff))
 	}
-	//写入mGoodsNumber(实际购买的数量):u2
-	if convertResult != convertSuccess {
-		realPoint = 0
-	}
-	opData = append(opData, byte((realPoint&0xff00)>>8), byte(realPoint&0xff))
 	response.OpData = opData
 	return response
 }
